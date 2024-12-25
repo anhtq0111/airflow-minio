285dac5150d5ffd16ef92fc15b7250a8b19b4c0b
anhtq0111 <anh.tq194225@gmail.com>
2024-12-25 11:17:34 +0700

    fix dag

--- airflow_minio.py
+++ airflow_minio.py
@@ -8,33 +8,31 @@ source_file = "airflow_minio.txt"
 # The destination bucket and filename on the MinIO server
 bucket_name = "airflow-bucket"
 destination_file = "test.txt"
+
+def get_minio_client():
+    return Minio(
+        endpoint="192.168.1.18:32023",
+        access_key="airflow-minio",
+        secret_key="airflow-minio",
+        secure=False
+    )
+
 @dag(schedule=None, start_date=pendulum.datetime(2024, 1, 1, tz="UTC"), catchup=False)
 def demo_airflow_minio():
-    @task
-    def connect_to_minio():
-        # try:
-        client = Minio(endpoint= '192.168.1.18:32023',
-                    access_key='airflow-minio',
-                    secret_key='airflow-minio',
-                    secure=False
-                    )
-        # except  S3Error as exc:
-        #     print("error occurred.", exc)
-        return client
-    client = connect_to_minio()
 
     @task
-    def create_bucket(client):
+    def create_bucket():
+        client = get_minio_client()
         found = client.bucket_exists(bucket_name)
         if not found:
             client.make_bucket(bucket_name)
             print("Created bucket", bucket_name)
         else:
             print("Bucket", bucket_name, "already exists")
-    create_bucket(client)
 
     @task
-    def upload_file(client, bucket_name, source_file, destination_file):
+    def upload_file(bucket_name, source_file, destination_file):
+        client = get_minio_client()
         client.fput_object(
             bucket_name, destination_file, source_file,
         )
@@ -42,7 +40,7 @@ def demo_airflow_minio():
             source_file, "successfully uploaded as object",
             destination_file, "to bucket", bucket_name,
         )
-    upload_file(client, bucket_name, source_file, destination_file)
+    create_bucket() >> upload_file(bucket_name, source_file, destination_file)
 
 airflow_minio = demo_airflow_minio()
 
