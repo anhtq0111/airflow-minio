0f95cae6250d7cf8e1bea2205d8a2c4587f5c08f
anhtq0111 <anh.tq194225@gmail.com>
2025-01-14 10:41:42 +0700

    test airbyte

--- airflow_airbyte.py
+++ airflow_airbyte.py
@@ -1,14 +1,21 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
 
-with DAG(dag_id='trigger_airbyte_job_example',
+@dag(dag_id='trigger_airbyte_job_example',
          default_args={'owner': 'airflow'},
          schedule_interval='@daily',
          start_date=days_ago(1)
-    ) as dag:
+) 
+def trigger_airbyte_job_example():
+    @task()
+    def start():
+        print("Start")
 
-    money_to_json = AirbyteTriggerSyncOperator(
+    sync_job = AirbyteTriggerSyncOperator(
         task_id='airbyte_example',
         airbyte_conn_id='airbyte_conn_example',
         connection_id='cba5d8de-32e6-4235-b167-1cd275dd7069',
@@ -16,3 +23,11 @@ with DAG(dag_id='trigger_airbyte_job_example',
         timeout=3600,
         wait_seconds=3
     )
+
+    @task()
+    def end():
+        print("End")
+
+    start() >> sync_job >> end()
+
+trigger_airbyte_job_example()
