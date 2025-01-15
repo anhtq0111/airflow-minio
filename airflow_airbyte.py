3a5ffec5d34fe06f91b64fda64b7960ea48f1a59
anhtq0111 <anh.tq194225@gmail.com>
2025-01-15 14:28:54 +0700

    test airbyte

--- airflow_airbyte.py
+++ airflow_airbyte.py
@@ -1,6 +1,6 @@
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
-from airflow.providers.airbyte.triggers.airbyte import AirbyteSyncTrigger
+# from airflow.providers.airbyte.triggers.airbyte import AirbyteSyncTrigger
 from airflow.decorators import dag, task
 from datetime import datetime, timedelta
 
@@ -18,21 +18,23 @@ def trigger_airbyte_job_example():
         task_id='airbyte_example',
         airbyte_conn_id='airbyte',
         connection_id='628a1b08-bf3b-4a02-87b7-76e94e1d5fb9',
-        asynchronous=True,
+        asynchronous=False,
         timeout=3600,
         wait_seconds=3
     )
 
-    airbyte_sensor = AirbyteJobSensor(
-        task_id='asensor_example',
-        airbyte_conn_id='airbyte',
-        airbyte_job_id=airbyte_sync_job.output
-    )
+    # airbyte_sensor = AirbyteJobSensor(
+    #     task_id='asensor_example',
+    #     airbyte_conn_id='airbyte',
+    #     airbyte_job_id=airbyte_sync_job.output
+    # )
 
     @task()
     def end():
         print("End")
 
-    start() >> airbyte_sync_job >> airbyte_sensor >> end()
+    start() >> airbyte_sync_job  >> end()
+    # start() >> airbyte_sync_job >> airbyte_sensor >> end()
+
 
 trigger_airbyte_job_example()
