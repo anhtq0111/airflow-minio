07aaeb4cc71cc6b2aab2254b40876500340f6efa
anhtq0111 <anh.tq194225@sis.hust.edu.vn>
2025-03-19 14:34:16 +0700

    demo upload v2

--- upload_manifest_minio.py
+++ upload_manifest_minio.py
@@ -7,12 +7,12 @@ from kubernetes.client import models as k8s
 
 # The destination bucket and filename on the MinIO server
 volume = k8s.V1Volume(
-    name="dbt-storage",
+    name="dbt",
     persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="dbt-storage-pvc"),
 )
 
 volume_mount = k8s.V1VolumeMount(
-    name="dbt-storage", mount_path="/dbt/target", sub_path=None
+    name="dbt", mount_path="/dbt/target", sub_path=None
 )
 
 
